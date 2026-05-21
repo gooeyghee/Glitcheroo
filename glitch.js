f37cac53ea0ee015fdd7ed089a4818416d6528a7
gooeyghee <guilhermejosenunes02@gmail.com>
2026-05-21 14:08:53 +0200

    Archive now working. Random Bytes function changed to mean the randombytes input. Selection preview gone in favour of only 3D shape.

--- glitch.js
+++ glitch.js
@@ -9,12 +9,14 @@ let input1 //randombyte1
 let input2 // randombyte2
 let input3 // textureX
 let input4 // textureY
-let random // randomize
+let randomBytesButton // randomize
 let randombytes = false
 let checkbox1 // rotationX
 let checkbox2 // rotationY
 let checkbox3 // rotationZ
 let shapeSelect
+
+let archiveEntry
 
 //position of the pixel range
 let positionX
@@ -29,6 +31,7 @@ let shapeNum = 1 // for the shapes toggle
 //archiving patterns
 let selection
 let selections = []
+let selectionsDOM = []
 let pushButton
 let clearButton
 
@@ -36,12 +39,11 @@ let fileInput
 
 let textureImg
 
-
 let rangeSize
 
 function preload(){
 	
-	img = new loadImage("data/images/gui.jpg")
+	img = loadImage("data/images/gui.jpg")
 	
 }
 
@@ -55,7 +57,6 @@ function setup() {
 	//capture = createCapture(VIDEO)
 	
 	//capture.hide()
-
 	guiHolder = createDiv() 
 	    .class('guiHolder') // add class for styling via CSS 
 
@@ -70,28 +71,37 @@ function setup() {
 		createDiv() 
 	    .parent(guiHolder) // place inside gui div 
 	    .class('label') // class for CSS
-	
-	//various inputs
-	inputByte1 = createInput(6) // randomByte
+
+	fileInput = createFileInput(handleImage)
+			.parent(guiHolder)
+			.class('button')
+
+	randomByteDiv = createDiv()
 		.parent(guiHolder)
-		.class('button')
-	inputByte2 = createInput(7) // randomByte
-		.parent(guiHolder)
-		.class('button')
+		.class('randomByteDiv')
+		
+		//various inputs
+
+	randomBytesButton = createButton("RANDOM BYTES!")
+			.parent(randomByteDiv)
+			.class('button')
+			
+	inputByte1 = createInput(floor(random(0,100))) // randomByte
+		.parent(randomByteDiv)
+		.style('margin-right:5px; margin-left: 10px;'
+		)
+
+	inputByte2 = createInput(floor(random(0,100))) // randomByte
+		.parent(randomByteDiv)
+
 	slideRangeSize = createSlider(1,300,50)
 		.parent(guiHolder)
 		.class('slider')
-	random = createButton("RANDOM BYTES!")
-		.parent(guiHolder)
-		.class('button')
 
 	shapeSelect = createSelect()
 		.parent(guiHolder)
 		.class('button')
 
-	fileInput = createFileInput(handleImage)
-		.parent(guiHolder)
-		.class('button')
 
 
 	//dropdown for the 3d shape selection
@@ -105,7 +115,6 @@ function setup() {
 	//glitch.pixelate(1)
 	
 	//toggles random bytes animation on and off
-	random.mousePressed(switchRandomBytes)
 	
 	frameRate(30)
 
@@ -126,14 +135,35 @@ function setup() {
 	checkbox3 = createCheckbox("Rotate Z",true)
 	 	.parent(guiHolder)
 		.class('checkbox')
-
-	pushButton = createButton("PUSH")
+	
+	
+	archiveHolder = createDiv()
 		.parent(guiHolder)
+		.class('archiveHolder')
+
+	archiveHolderNav = createDiv()
+		.parent(archiveHolder)
+
+	archiveHolderTextures = createDiv()
+		.parent(archiveHolder)
+
+
+	savedPatterns = createDiv('SAVED PATTERNS')
+		.parent(archiveHolderNav)
+		.style('padding:10px;')
+
+	pushButton = createButton("SAVE")
+		.parent(archiveHolderNav)
 		.class('button')
 	
 	clearButton = createButton("CLEAR")
-		.parent(guiHolder)
+		.parent(archiveHolderNav)
 		.class('button')
+
+	saveFile = createButton('DOWNLOAD')
+		.parent(archiveHolderNav)
+		.class('button')
+	
 	
 	pushButton.mousePressed(pushArchive)
 	clearButton.mousePressed(clearArchive)
@@ -144,63 +174,149 @@ function setup() {
     outline: none; 
     box-sizing: border-box; 
   } 
+
+	body{
+		overflow: hidden;
+		}	
+
     .guiHolder{ 
       background-color: black;
 	  width: 25%;
+	  height:95%;
       position: fixed;
-      right: 50px; 
-      top: 50px; 
+      right: 10px; 
+      top: 10px;
       border: 2px solid #fff; 
-      border-radius: 10px; 
       padding: 20px;
+	  overflow: hidden;
     } 
+
+	.archiveHolder{
+	  background-color: black;
+	  height:226px;
+      right: 50px; 
+      top: 300px; 
+      border: 2px solid #fff;
+	  padding-top:20px;
+      padding-left: 20px;
+	  padding-bottom: 20px;
+	  color:#fff; 
+      font-family:sans-serif; 
+	  overflow-y:auto;
+      font-size:24pt;
+	  text-align:center;
+      padding:5px; 
+	  float: left;
+	}
+
+	.randomByteDiv {
+
+      	font-family:sans-serif; 
+      	font-size:24pt;
+      	padding:5px;
+		justify-content: space-evenly;
+
+	}
+
     .label{ 
       color:#fff; 
       font-family:sans-serif; 
       font-size:36pt;
       padding:5px; 
     } 
+	
     .slider{ 
       width: 100%;
+	  background: black;
     } 
+
     
     .button{
-    	width: 100%;
     	margin-bottom: 10px;
 		background-color: black;
 		color: #fff;
 		border: 2px solid #fff;
-		border-radius: 20px;
-		padding:10px;
+		padding:7.5px;
+		margin-right: 5px;
     }
 
 	.button:hover {
-    	width: 100%;
     	margin-bottom: 10px;
 		background-color: #fff;
 		color: black;
-		border: 2px solid #fff;
-		border-radius: 20px;
-		padding:10px;
+		border: 2px solid #fff;x
 		transition: 0.5s;
     }
 
-	.input{
+	input[type=text]{
+		width:25%;
 	}
 
 	.checkbox{
-	    width: 100%;
     	margin-bottom: 10px;
-		background-color: black;
 		font-family: sans-serif;
 		font-size: 10pt;
 		color: #fff;
 		border: 2px solid #fff;
-		border-radius: 20px;
 		padding:10px;
 	}
 
+	.archive {
+		padding-bottom:0px;
+		margin-right: 10px;
+		border: 2px solid hidden;
+		float:left;
+
+	}
+	
+	.tempImg{
+	
+		width: 75px;
+	
+	}
+
+	.tempImg:hover {
+		padding-bottom:0px;
+		border: 2px solid #fff;
+
+		}
+
+	.clearScreen {
+		width:100px;
+		height:
+		color:white;
+		display:none;
+		position: relative;
+		}
+
+	.closeButton {
+		margin: 5px;
+		padding: 5px;
+		background-color: black;
+		font-family: sans-serif;
+		font-size: 10pt;
+		color: white;
+		float:right;
+		border: 0px;
+		
+	}
+
+	.closeButton:hover {
+		background-color:white;
+		color:black;
+		border: 0px;
+	}
+
+	.archiveHolderNav {
+		position:fixed;
+		top: 0;
+	}
+
     `) 
+
+	saveFile.mousePressed(function(){save(textureImg,'myPattern.jpg')})
+
+	grabArchive()
 
 }
 
@@ -225,6 +341,7 @@ function draw() {
 		glitchScreen()
 
 	}
+
 
 }
 
@@ -251,16 +368,34 @@ function mousePressed(){
 }
 
 function pushArchive(){
-	
-		tempSelection = new Selection(850,300 + selections.length * 125)
-		selections.push(tempSelection)
-	
+
+	let imageData = textureImg.canvas.toDataURL()
+
+		archiveEntry = {
+       	 image: imageData
+    	}
+
+	selections.push(archiveEntry)
+    storeItem('selections', selections)
+
+	grabArchive()
 }
 
-function clearArchive(){
+
+function clearArchive(index){
 	
-		selections.splice(-1)
+	selections.splice(index)
+    storeItem('selections', selections)
+	grabArchive()
+			
+}
+
+function deleteTexture(index){
 	
+	selections.splice(index, 1)
+    storeItem('selections', selections)
+	grabArchive()
+			
 }
 
 
@@ -275,13 +410,13 @@ function glitchScreen() {
 	inputByte1.changed(glitchSample)
 	inputByte2.changed(glitchSample)
 
+	randomBytesButton.mousePressed(function(){let r1 = random(1,100); let r2 = random(1,100); inputByte1.value(floor(r1));
+								   inputByte2.value(floor(r2)); randomByte1 = floor(r1); randomByte2 = floor(r2); glitchSample()})
+
 	//the actual glitching
 	glitch.resetBytes()
 	glitch.replaceBytes(randomByte1, randomByte2); // find + replace all
 	
-	if (randombytes == true){
-	glitch.randomBytes(3)
-	}
 	
 	//build and display image
 	glitch.buildImage(); // creates image from glitched data
@@ -306,15 +441,18 @@ function Shape3D(){
 
 				textureImg  = glitch.image.get(positionX,positionY,rangeSize,rangeSize)
 
+
 		}
 	}
+
+
 
 	slideRangeSize.changed(function(){ textureImg = glitch.image.get(positionX,positionY,rangeSize,rangeSize) })
 
 	push()
 	noStroke()
 	texture(textureImg)
-	translate(width/2,height/2)
+	translate(width/2+175,200)
 
 	if (checkbox1.checked()){
 		rotateX(frameCount*0.04)
@@ -330,15 +468,15 @@ function Shape3D(){
 
 	//Telling it what shape to generate
 	if (shapeNum == 1) {
-		box(200)
+		box(150)
 	}
 	
 	if (shapeNum == 2) {
-		cylinder(200)
+		cylinder(150)
 	}
 	
 	if (shapeNum == 3){
-		sphere(200)
+		sphere(150)
 	}
 
 	if (shapeNum == 4){
@@ -346,16 +484,6 @@ function Shape3D(){
 	}
 	
 	pop()
-}
-
-function switchRandomBytes(){
-	
-	if (randombytes == true){
-		randombytes = false
-	} else {
-		randombytes = true
-	}
-	
 }
 
 class Selection {
@@ -366,7 +494,7 @@ class Selection {
 		this.y = y
 		this.rangeSize = rangeSize
 		
-		if (mousePressed){
+		if (mouseIsPressed){
 		this.selectionRange = glitch.image.get(positionX,positionY,this.rangeSize,this.rangeSize)
 		}
 
@@ -375,60 +503,20 @@ class Selection {
 
 	}
 	
-	show(){
-		
-
-		image(this.selectionRange,this.x,this.y,this.archiveSize,this.archiveSize)
-		
-		if(dist(mouseX,mouseY,this.x,this.y) <= rangeSize){
-		
-			if (mousePressed){
-				
-				texture(this.selectionRange)
-				
-			}
-			
-			push()
-			noFill()
-			strokeWeight(5)
-			rect(this.x,this.y,this.previewSize,this.previewSize)
-			pop()
-			
-		}
-	}
-	
-	showPreview(){
-		
-		image(this.selectionRange,this.x,this.y,this.previewSize,this.previewSize)
-	}
 	
 }
 
 function selectFunction(){
-
-	selection = new Selection()
-	
-	for(let s of selections){
-		
-			s.show()
-			
-	}
 	
 	push()
 		noFill()
 		
 		if (mouseX < width/2 && mouseY < height) {
-		strokeWeight(5)
+		strokeWeight(2)
 		rect(mouseX-rangeSize/2,mouseY-rangeSize/2,rangeSize,rangeSize)
-	
 		}
 
 	pop()
-	
-	selection.x = width/2
-	selection.y = 0
-	selection.showPreview()
-
 }
 
 function handleImage(file) {
@@ -441,4 +529,60 @@ function handleImage(file) {
   } else {
     img = null;
   }
+}
+
+function grabArchive(){
+
+	archiveHolderTextures.html("")
+
+	let savedData = getItem('selections');
+
+	if(savedData){
+		selections = savedData
+
+		let c = 1
+		for(let entry of selections){
+
+			let imageContainer = createDiv()
+				imageContainer.class('archive')
+				imageContainer.id(`texture-${c}`)
+				imageContainer.parent(archiveHolderTextures)
+
+				
+				let clearScreen = createDiv()
+					clearScreen.parent(imageContainer)
+					clearScreen.class('clearScreen')
+				
+				let closeButton = createButton('x')
+					closeButton.parent(clearScreen)
+					closeButton.class('closeButton')
+
+			let tempImg = createImg(entry.image)
+				tempImg.parent(imageContainer)
+				tempImg.class('tempImg')
+			
+			
+			imageContainer.mouseOver(function(){
+				clearScreen.style('display:inline')
+			})
+
+			imageContainer.mouseOut(function(){
+				clearScreen.style('display:none')
+			})
+
+			let index = c - 1
+
+			closeButton.mousePressed(function(){deleteTexture(index)})
+
+			tempImg.mousePressed(function(){
+
+				loadImage(entry.image, function(img){
+					textureImg = img
+				})
+
+			})
+			c++
+		}
+	}
+
 }
